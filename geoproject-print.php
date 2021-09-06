eca6a5e74f31a31ee3a25dc46a46decfefd1a093
julientaq <julien@coko.foundation>
2021-09-06 12:40:06 +0200

    fix template creation

--- geoproject-print.php
+++ geoproject-print.php
@@ -17,6 +17,37 @@ if (!defined('ABSPATH')) {
 }
 
 
+//  try to avoid the creation of a book
+
+// function pagedjs_bookContent()
+// {
+//     register_post_type(
+//         'pagedjs-book',
+//         array(
+//             'labels' => array(
+//                 'name' => __('Book Content'),
+//                 'singular_name' => __('Book'),
+//                 'add_new' => __('Create a new book'),
+//                 'add_new_item' => __('Create a new book'),
+//                 'edit_item' => __('Edit book'),
+//             ),
+//             'exclude_from_search' => true,
+//             'has_archive' => false,
+//             'menu_position' => 51,
+//             'public' => true,
+//             'supports' => array('title'),
+//             'register_meta_box_cb' => 'css_metabox',
+//             'menu_icon' => 'dashicons-book',
+//         )
+
+//     );
+// }
+
+
+// 
+
+
+
 function pagedjs_css_custom_post_type()
 {
     register_post_type(
@@ -25,15 +56,16 @@ function pagedjs_css_custom_post_type()
             'labels' => array(
                 'name' => __('Book Templates'),
                 'singular_name' => __('Template'),
-                'add_new' => __('Create new template'),
-                'add_new_item' => __('Create new template'),
-                'edit_item' => __('Edit template'),
+                'add_new' => __('Create a new template'),
+                'add_new_item' => __('Create a new template'),
+                'edit_item' => __('Edit book template'),
             ),
             'exclude_from_search' => true,
             'has_archive' => false,
             'menu_position' => 50,
             'public' => true,
             'supports' => array('title'),
+            'menu_icon' => 'dashicons-book-alt',
             'register_meta_box_cb' => 'css_metabox'
         )
 
@@ -151,12 +183,8 @@ function template_metabox_display()
 {
 
     global $post;
-    // $templateName = get_post_meta( $post->ID,'templateName', true);
     $templateCSS = get_post_meta($post->ID, 'templateCSS', true);
     ?>
-        <!-- <label for="templateName">name of your template</label>
-        <input type="text" name="templateName" placeholder="my template name" class="widefat" value="<?php print $templateName ?>" required>
-        <br/><br/> -->
         <label for="templateCSS">the code</label>
         <textarea placeholder="my template CSS" name="templateCSS" rows="30" class="widefat"><?php print $templateCSS ?></textarea>
     <?php
@@ -166,6 +194,7 @@ function template_metabox_display()
 
 
 add_action('init', 'pagedjs_css_custom_post_type');
+// add_action('init', 'pagedjs_bookContent');
 
 
 // save the data
