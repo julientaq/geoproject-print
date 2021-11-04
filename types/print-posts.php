1c2811d7b90f8aaa4a7a16e1f31035aa0f37bfe2
julientaq <julien@coko.foundation>
2021-11-04 15:15:18 +0100

    add date

--- types/print-posts.php
+++ types/print-posts.php
@@ -3,19 +3,15 @@
 
     <header>
         <?php $meta_date = get_post_meta( get_the_ID(), 'meta-date', true ); ?>
-
-        <p class="date-entry"><?php echo($meta_date)?></p>
+        <?php $post_date = get_the_date( 'l j F Y' );  ?>
+        <p class="date-entry"><?php echo($post_date) ?></p>
         <h2><?php the_title() ?></h2>
-        <?php include 'reusable/getThumbnail.php' ?>
+        <figure><?php include 'reusable/getThumbnail.php' ?></figure>
     </header>
 
     <article>
         <?php the_content() ?>
     </article>
-
-
-
-
 
     <?php include 'reusable/tags.php' ?>
 
